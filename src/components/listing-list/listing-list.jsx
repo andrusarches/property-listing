972454d88c2d80af17d1ac724e3aaf0ec4e1a482
andrusarches <64173881+andrusarches@users.noreply.github.com>
2021-06-10 13:34:24 +0300

    Finalized

--- src/components/listing-list/listing-list.jsx
+++ src/components/listing-list/listing-list.jsx
@@ -6,12 +6,12 @@ import './listing-list.scss';
 import ListingCard from '../listing-card/listing-card';
 
 const ListingList = ({propertyData, cardLimit}) => {
-
+     
     return (
         <ul className="listing-list">
             {propertyData.length > 0
                 ? propertyData.slice(0, cardLimit).map((item) => (<ListingCard key={item.id} data={item} />))
-                : <h3 className="listing-list__error-message">Sorry, nothing was found!</h3>    
+                : onEmptyDataArray()   
             }
         </ul>
     );
